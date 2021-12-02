8e041a7036956c08193f77582b77613d24e78bae
mark48853 <sathanalao@hotmail.com>
2021-12-02 15:35:13 +0700

    fixes bugs

--- Project/preview.py
+++ Project/preview.py
@@ -12,15 +12,27 @@ def preview(Reply_token , message):
     mx_page = len(book['images']['pages'])
   #  print(mx_page)
 
+    sauce = requests.get(f"https://nhentai.net/g/{message}/")
+    soup = BeautifulSoup(sauce.text,"html.parser")
+    a = soup.find_all('img')[2]['data-src']
+
+    sauce = ""
+    ar = a.split('/')
+    br = False
+    for components in ar:
+        sauce = sauce+components+'/'
+        if(br):
+            break
+        if(components == "galleries"):
+            br=True
+
     payload = []
 
     for i in range(1,mx_page,int(mx_page/10)+1):
-        r = requests.get(f"https://nhentai.net/g/{message}/{i}/")
-        soup = BeautifulSoup(r.text,"html.parser")
-        a = soup.find_all('img')
-        payload.append(con3(a[1]['src']))
-        print(a[1]['src'])
-        if(len(payload)==4):
+        src = sauce+str(i)+"t.jpg"
+        payload.append(con3(src))
+        print(src)
+        if(len(payload)==6):
             break
 
 
