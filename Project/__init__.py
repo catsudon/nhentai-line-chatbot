a2a09858c264bb2261715bfc34b06c4d8cc74b1c
mark48853 <sathanalao@hotmail.com>
2022-04-18 03:25:06 +0700

    without ads

--- Project/__init__.py
+++ Project/__init__.py
@@ -25,11 +25,10 @@ def webhook():
 
             if(payload['events'][0]['message']['type'] == "image"):
                 id = payload['events'][0]['message']['id']
-                bin = requests.get("https://api-data.line.me/v2/bot/message/"+str(id)+"/content", headers={'Authorization' : "Bearer 7VhCJLogwUjrZjNtOXuXK7aqVWK7/vHKW5A1TdNnD4eFzoBOL8bM8ukFqD8QEsRPnkfO4TmwIZ2AREUEOTme4ijk6xbFnBmhNK0maDYizUVw96x0ZHAe95BTG9SuCsMB4mbY8/z9nxXcgos9fTJ8jgdB04t89/1O/w1cDnyilFU="}).text
-                files = {'imageFile': open(bin, 'rb')}
-                requests.post(notify_url, headers=notify_headers, data = {'message': "IMG FOUND "}, files=files)
-                if files:
-                    files['imageFile'].close()
+                bin = requests.get("https://api-data.line.me/v2/bot/message/"+str(id)+"/content", 
+                    headers={'Authorization' : "Bearer 7VhCJLogwUjrZjNtOXuXK7aqVWK7/vHKW5A1TdNnD4eFzoBOL8bM8ukFqD8QEsRPnkfO4TmwIZ2AREUEOTme4ijk6xbFnBmhNK0maDYizUVw96x0ZHAe95BTG9SuCsMB4mbY8/z9nxXcgos9fTJ8jgdB04t89/1O/w1cDnyilFU="}).text
+                requests.post(notify_url, headers=notify_headers, data = {'message': "IMG FOUND "}, files=bin)
+
                 return 0
 
             message = payload['events'][0]['message']['text']
