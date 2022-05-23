7e9b2fd87e7b820db3aea0bc6517d7d8f1b9ec28
mark48853 <sathanalao@hotmail.com>
2022-05-23 08:41:36 +0700

    link fixes back

--- modules/nhentai.py
+++ modules/nhentai.py
@@ -1,10 +1,12 @@
 import requests,json
-
+import time
 def search(query=None,page=1):
     if query is not None:
-        url = f"https://nhentai.to/api/galleries/search?query={query}&page={page}&sort=popular"
+        url = f"https://nhentai.net/api/galleries/search?query={query}&page={page}&sort=popular"
     else:
-        url = f"https://nhentai.to/api/galleries/all?page={page}&sort=popular"
+        url = f"https://nhentai.net/api/galleries/all?page={page}&sort=popular"
+    requests.get(url)
+    time.sleep(1)
     requests.get(url)
     res = requests.get(url).content.decode()
     return res
@@ -12,7 +14,10 @@ def search(query=None,page=1):
 def getBookById(_id):
     if _id == None:
         return "id required."
-    url = f"https://nhentai.to/api/gallery/{_id}"
+    url = f"https://nhentai.net/api/gallery/{_id}"
+    
+    requests.get(url)
+    time.sleep(1)
     requests.get(url)
     res = requests.get(url).content.decode()
     return res
