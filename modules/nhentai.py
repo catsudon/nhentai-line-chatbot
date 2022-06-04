f8ad3e386d7ac27ccf421d0661139e04163d6f5d
mark48853 <sathanalao@hotmail.com>
2022-06-04 21:00:26 +0700

    a

--- modules/nhentai.py
+++ modules/nhentai.py
@@ -56,12 +56,12 @@ def getBookById(_id):
     if _id == None:
         return "id required."
     url = f"https://nhentai.net/api/gallery/{_id}"
-    url = f"https://hifumin.app/h/{_id}"
+    url = f"https://nhentai.xxx/g/{_id}"
     
     res = requests.get(url)
     soup = BeautifulSoup(res.content, "html.parser")
-    print(soup)
-    img = soup.find_all('img')[0]
+    #print(soup)
+    img = soup.find_all('img')[1]
     
     book = {
         "cover_url" : img['src'],
