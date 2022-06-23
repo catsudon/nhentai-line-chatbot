3c6137389a6a7d798f1dbf4e3116c52fca2f49f3
mark48853 <sathanalao@hotmail.com>
2022-06-23 15:03:11 +0700

    add doujin suggestion ;)

--- modules/nhentai.py
+++ modules/nhentai.py
@@ -4,7 +4,7 @@ def search(query="แนะนำ",page=1):
     if not "แนะนำ" in query and query is not "/":
         url = f"https://nhentai.net/api/galleries/search?query={query}&page={page}&sort=popular"
     else:
-        url = f"https://nhentai.net/api/galleries/search?sort=popular&uploaded%3C1d&query=/&page={page}"
+        url = f"https://nhentai.net/api/galleries/search?sort=popular&query=uploaded:<2d"
 
     res = requests.get(url).content.decode()
     return res
