18f93d419aa4442531200a4b48c76b02d55f9e43
mark48853 <sathanalao@hotmail.com>
2022-07-06 19:54:42 +0700

    graphQL url fixes

--- modules/nhentai.py
+++ modules/nhentai.py
@@ -1,7 +1,7 @@
 import requests,json
 
 def search(query="แนะนำ",page=1):
-    if not "แนะนำ" in query and query is not "/":
+    if not "แนะนำ" in query and query != "/":
         url = f"https://nhentai.net/api/galleries/search?query={query}&page={page}&sort=popular"
     else:
         url = f"https://nhentai.net/api/galleries/search?sort=popular&query=uploaded:<2d"
@@ -12,11 +12,16 @@ def search(query="แนะนำ",page=1):
 def getBookById(_id):
     if _id == None:
         return "id required."
-    url = "https://api.hifumin.app/"
+    url = 'https://api.hifumin.app/v1/graphql'
     query = '{nhentai {  by(id:'+str(_id)+', channel:"HIFUMIN_FIRST")  {    id    mediaId    title {display:pretty}    images {cover {t,w,h}}  }}}'
     
-    res = json.loads(requests.post(url, json={'query':query}).text)
+    head = {
+        "Accept-Encoding": "gzip, deflate, br"
+    }
+    r = requests.post(url, json={'query':query})
+    print(r.status_code, r.text)
+    res = json.loads(r)
     return res
-
+    
 if __name__ == "__main__":
     print(getBookById(245031))
